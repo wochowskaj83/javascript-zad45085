cce294fa8b00ddc0a80bb63d972b11f735d40812
jolaw <wochowskaj@gmail.com>
2023-04-11 19:10:12 +0200

    rozwiazania zadania

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = 7;
+let secondNumber = 11;
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+
+    return result;
+}
+let wynikDodawania = sum(firstNumber, secondNumber)
+console.log(wynikDodawania)
